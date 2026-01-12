4387d3ea6b4e62377ea6e5ea21296c0dbbc5ffaf
Om-katkar <katkarjb783@gmail.com>
2026-01-12 10:15:21 +0530

    initial commit

---
+++ src/main/java/com/draken_bigbro/biome_weapon/BiomeWeaponDataGenerator.java
@@ -0,0 +1,11 @@
+package com.draken_bigbro.biome_weapon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BiomeWeaponDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
